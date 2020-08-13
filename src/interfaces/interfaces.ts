c828fe941f367f8dd52c1d189d12c2a80d5d2133
gilmarferrini <ferrinigilmar@gmail.com>
2020-08-12 21:03:06 -0300

    refactoring

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -2,6 +2,7 @@ interface IPessoa {
   nome: string;
   idade?: number;
   [prop: string]: any; // atributo dinamico
+  saudar?:(sobrenome: string) => void; // retorna nada ou undefined
 }
 
 function saudarComOla(pessoa: IPessoa) {
@@ -12,11 +13,15 @@ function mudarNome(pessoa: IPessoa) {
   pessoa.nome = 'Joana';
 }
 
-const pessoa = {
+const pessoa: IPessoa = {
   nome: 'João',
   idade: 27,
+  saudar(sobrenome: string) {
+    console.log(`Olá meu nome é ${this.nome} ${sobrenome}`);
+  }
 };
 
 saudarComOla(pessoa);
 mudarNome(pessoa);
 saudarComOla(pessoa);
+pessoa.saudar('Skywalker');
